e76d40768e4e34cb51a192ba8ba96b96318f876a
callin <callin@elevisor.com>
2017-08-25 19:26:18 +0900

    테이블 생성 한번만 되도록 설정 변경 https://stackoverflow.com/questions/21968965/disable-table-recreation-in-spring-boot-application https://stackoverflow.com/questions/29809219/on-duplicate-key-update-feature-in-h2

--- CypherBrowser/src/main/resources/data.sql
+++ CypherBrowser/src/main/resources/data.sql
@@ -1,5 +1,5 @@
-insert into `MEMBER_ROLE` (`ROLE_ID`, `ROLE`) values (21, 'ADMIN');
-insert into `MEMBER_ROLE` (`ROLE_ID`, `ROLE`) values (22, 'USER');
+merge into `MEMBER_ROLE` (`ROLE_ID`, `ROLE`) values (21, 'ADMIN');
+merge into `MEMBER_ROLE` (`ROLE_ID`, `ROLE`) values (22, 'USER');
 
-insert into `MEMBER` (`ID`, `PASSWORD`, `ROLE_ID`) values ('agraph', 'agraph', 21);
-insert into `MEMBER` (`ID`, `PASSWORD`, `ROLE_ID`) values ('test01', 'test01', 22);
+merge into `MEMBER` (`ID`, `PASSWORD`, `ROLE_ID`) values ('agraph', 'agraph', 21);
+merge into `MEMBER` (`ID`, `PASSWORD`, `ROLE_ID`) values ('test01', 'test01', 22);
