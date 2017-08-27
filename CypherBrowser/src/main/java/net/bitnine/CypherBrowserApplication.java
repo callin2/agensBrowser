29bbdd870d2d25343990d0c61edc8a7b46d94309
callin <callin@elevisor.com>
2017-08-27 12:08:15 +0900

    servlet initializer 통합

--- CypherBrowser/src/main/java/net/bitnine/CypherBrowserApplication.java
+++ CypherBrowser/src/main/java/net/bitnine/CypherBrowserApplication.java
@@ -2,11 +2,18 @@ package net.bitnine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CypherBrowserApplication {
+public class CypherBrowserApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CypherBrowserApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CypherBrowserApplication.class);
 	}
 }
